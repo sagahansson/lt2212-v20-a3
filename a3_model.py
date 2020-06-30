9baefacd9cb7ce3e48e641b2107d9a8b186022f4
Saga Hansson <gushansad@GU.GU.SE@mltgpu.flov.gu.se>
2020-06-30 16:43:43 +0200

    actual garbage but it's running

--- a3_model.py
+++ a3_model.py
@@ -4,9 +4,9 @@ import argparse
 import numpy as np
 import pandas as pd
 import torch
-from torch import nn
-from torch import optim
+from torch import nn, autograd, optim
 import random
+import torch.nn.functional as F
 # Whatever other imports you need
 
 # You can implement classes and helper functions here too.
@@ -25,22 +25,24 @@ def sampling(batchsize, df):
         else: # if t_f is 0 
             second_author = authors.pop(random.randrange(0,len(authors))) # skaffa en annan random author
 
-        author1_tensor = torch.from_numpy(df[df["Author"] == first_author].sample(n=1).drop(["Train/Test", "Author"], axis=1).values) # picking a random row where Author = first_author, dropping the Train/Test and Author columns, converting to a ndarray, then to a tensor
-        author2_tensor = torch.from_numpy(df[df["Author"] == second_author].sample(n=1).drop(["Train/Test", "Author"], axis=1).values)
+        author1_tensor = torch.FloatTensor(df[df["Author"] == first_author].sample(n=1).drop(["Train/Test", "Author"], axis=1).values) # picking a random row where Author = first_author, dropping the Train/Test and Author columns, converting to a ndarray, then to a tensor
+        author2_tensor = torch.FloatTensor(df[df["Author"] == second_author].sample(n=1).drop(["Train/Test", "Author"], axis=1).values)
     
-        tensors = torch.cat((author1_tensor, author2_tensor), 0) # concatenating the two author tensors
+        #tensors = torch.cat((author1_tensor, author2_tensor), 0) # concatenating the two author tensors
         
-        tens_labels.append((tensors, t_f)) # adding the label of the tensors i e if they're by the same auhtor or not
+        tensors = autograd.Variable(torch.FloatTensor((author1_tensor + author2_tensor)))
+        
+        tens_labels.append((tensors, torch.FloatTensor([t_f]))) # adding the label of the tensors i e if they're by the same auhtor or not
         
     return tens_labels
         
     
     
 class NN(nn.Module):
-    def __init__(self, inS  ) # hiS, noL
-# ins = input size, hs = hidden size, noL = non linearity
+    def __init__(self, inS): # hiS, noL
+# inS = input size, hS = hidden size, noL = non linearity
         super().__init__()
-        self.l = nn.Linear(inS, 1)
+        self.lyr = nn.Linear(inS, 1)
         self.sigmoid = nn.Sigmoid()
       #  if noL != None:
       #      if noL == 'relu':
@@ -64,57 +66,77 @@ class NN(nn.Module):
       #          x = self.l1(x)
       #          x = self.noL(x)
       #          x = self.l2(x)
-        x = self.l(x)
+        x = self.lyr(x)
         x = self.sigmoid(x)
         return x
     
-    def training(epochs, iterations, batchsize, df):
-        net = NN()
-        for epoch in range(epochs):
-            for iteration in range(iterations):
-                samples = sampling(batchsize, df)
-                tensor = [x[0] for x in samples]
-                label = [x[1] for x in samples]
-                
+def nn_training(traindf, input_sz=160, epochs=3, iterations=16, batchsize=10, learning_rate=0.001):
+    net = NN(inS=input_sz)
+    opt = optim.Adam(params=net.parameters(), lr=learning_rate)
+    lossfunct = nn.BCELoss()
+    for epoch in range(epochs):
+        for iteration in range(iterations):
+            samples = sampling(batchsize, df)
 
+            tensors = [x[0] for x in samples]
+            labels = [x[1] for x in samples]
+
+            tensors = autograd.Variable(torch.stack(tensors))
+
+            labels = autograd.Variable(torch.tensor(labels))
+            print("tensors/target: ", tensors)
+            out = net(tensors)
+            print(out)
+#            _, prediction = out.max(1)
+#            print("prediction: ", prediction)
+            print("target: ", labels)
+            loss = lossfunct(out, labels)
+            opt.zero_grad()
+            loss.backward()
+            print("loss: ", loss.data)
+
+            opt.step()
+
+    print("Training done.")
+
+def nn_testing(testdf, ):
+    pass
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Train and test a model on features.")
     parser.add_argument("featurefile", type=str, help="The file containing the table of instances and features.")
     # Add options here for part 3 -- hidden layer and nonlinearity,
     # and any other options you may think you want/need.  Document
     # everything.
-    parser.add_argument("--batchsize", "-B", dest=B, type=int, default=10, help="The size of the batch passed at each iteration.")
-    parser.add_argument("--epochs", "-E", dest=E, type=int, default=3, help="The number of epochs.")
-    parser.add_argument("--train_examples", "-TrEx", dest=TrEx, type=int, default=160, help="The number of examples the NN will train on.")
-    parser.add_argument("--test_examples", "-TeEx", dest=TeEx, type=int, default=40, help="The number of examples the NN will test on.")
+    parser.add_argument("--batchsize", "-B", dest="B", type=int, default=10, help="The size of the batch passed at each iteration.")
+    parser.add_argument("--epochs", "-E", dest="E", type=int, default=3, help="The number of epochs.")
+    parser.add_argument("--train_examples", "-TrEx", dest="TrEx", type=int, default=160, help="The number of random pairs + label the NN will train on.")
+    parser.add_argument("--test_examples", "-TeEx", dest="TeEx", type=int, default=40, help="The number of random pairs the NN will test on.")
     #parser.add_argument("--hidden_size", "-HS", dest=HS, type=int, default=None, help="The size of the hidden layer.")
     #parser.add_argument("--non_linearity", "-NL", dest=AS, type=str, default=None, choices=['relu', 'tanh'], help="Choice of either ReLU or Tanh".)
     
     # number of iterations is a function of batchsize and train examples: for 160 train examples and batchsize 10: 160/10 = 16 iterations to get through all the training data.  
     args = parser.parse_args()
-    inputsize = df.shape[1]-2 # INPUT SIZE TO NN != batchsize    
+    
+    print("Reading {}...".format(args.featurefile))
+    df = pd.read_csv(args.featurefile)
+    train_df = df[df["Train/Test"] == "Train"]
+    test_df = df[df["Train/Test"] == "Test"]
+    test_df.reset_index(inplace=True, drop=True)
+    print("Done reading.")
+    
+    
     
     B = args.B
     E = args.E
     TrEx = args.TrEx
     TeEx = args.TeEx
-    
     iterations = TrEx//B
-    
+    inputsize = (df.shape[1]-2) # INPUT SIZE TO NN != batchsize    
+
+    net = NN(inS=(inputsize))
+    optimizer = optim.SGD(net.parameters(), lr=0.01, momentum=0.5)
+    criterion = nn.BCELoss()
+        
     
 
-    print("Reading {}...".format(args.featurefile))
-
-    df = pd.read_csv(args.featurefile)
-    train_df = df[df["Train/Test"] == "Train"]
-    test_df = df[df["Train/Test"] == "Test"]
-    test_df.reset_index(inplace=True, drop=True)
-    
-    print("Done reading.")
-    
-    
-    
-    sampling(1, train_df)
-    
-    # implement everything you need here
-    
+#    nn_training(train_df)
